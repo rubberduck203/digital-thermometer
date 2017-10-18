61e8c3269a43820431ecb19f374468ab89cb9e39
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2017-10-18 13:25:37 -0400

    Enable shift register output

--- src/ShiftRegister.cpp
+++ src/ShiftRegister.cpp
@@ -1,8 +1,12 @@
 #include "ShiftRegister.h"
 
+const uint8_t registerMask = 0b00011111;
+const uint8_t outputEnablePin = 1;
+
 ShiftRegister::ShiftRegister(IOPort_t& port)
 {
-    port.Direction = 0x1F; //set 0-4 to output
+    port.Direction |= registerMask; //set 0-4 to output
+    port.Data = (port.Data & ~registerMask) | (registerMask & ~(1 << outputEnablePin));
 }
 
 void ShiftRegister::writeByte(uint8_t data)
--- test/ShiftRegisterTests.cpp
+++ test/ShiftRegisterTests.cpp
@@ -37,9 +37,35 @@ TEST(ShiftRegisterDriverTests, OnInitialization_PinsSetupAsOutput)
     BYTES_EQUAL(0b00011111, shiftRegPort.Direction);
 }
 
-IGNORE_TEST(ShiftRegisterDriverTests, OnIntialization_OutputEnableIsDrivenLow)
+TEST(ShiftRegisterDriverTests, OnInitalization_DirectionFor_PinsFiveSixAndSeven_AreNotModified)
 {
-    
+    IOPort_t shiftRegPort;
+    shiftRegPort.Direction = 0xFF;
+    ShiftRegister shiftRegister(shiftRegPort);
+
+    BYTES_EQUAL(0xFF, shiftRegPort.Direction);
+}
+
+TEST(ShiftRegisterDriverTests, OnInitalization_DataOn_PinsFiveSixAndSeven_IsNotModified)
+{
+    IOPort_t shiftRegPort;
+    shiftRegPort.Data = 0x00;
+    ShiftRegister shiftRegister(shiftRegPort);
+
+    BITS_EQUAL(0b00011101, shiftRegPort.Data, 0xFF);
+    BYTES_EQUAL(0b00011101, shiftRegPort.Data);
+}
+
+TEST(ShiftRegisterDriverTests, OnIntialization_OutputEnableIsDrivenLow)
+{
+    IOPort_t shiftRegPort;
+    shiftRegPort.Direction = 0x00;
+    shiftRegPort.Data = 0xFF;
+    ShiftRegister shiftRegister(shiftRegPort);
+
+    BITS_EQUAL(0b11111101, shiftRegPort.Data, 0xFF);
+    BYTES_EQUAL(0b11111101, shiftRegPort.Data);
+    // it would be far nicer to have bool shiftRegister.OutputEnabled()
 }
 
 IGNORE_TEST(ShiftRegisterDriverTests, WritesAByte)
