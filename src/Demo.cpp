a63b0ebe3d63774389ac0a189032423e8d698dd4
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2017-10-21 09:01:08 -0400

    Quick program to verify we're pushing bits in the right order.

--- src/Demo.cpp
+++ src/Demo.cpp
@@ -2,19 +2,39 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+// /* Shift Register Driver Test 1 */
+// int main(void) 
+// {   
+//     IOPort_t* shiftRegisterPort = (IOPort_t*)&DDRB;
+//     ShiftRegister shiftRegister(*shiftRegisterPort);
+//     for(;;)
+//     {
+//         for(uint8_t i = 0; i < 255; i++)
+//         {
+//             shiftRegister.writeByte(i);
+//             shiftRegister.latch();
+//             _delay_ms(1000);
+//         }
+//     }
+//     return 0;
+// }
+
 int main(void) 
 {   
-    //TODO: setup and loop
     IOPort_t* shiftRegisterPort = (IOPort_t*)&DDRB;
     ShiftRegister shiftRegister(*shiftRegisterPort);
+
+    int i = 0;
     for(;;)
     {
-        for(uint8_t i = 0; i < 255; i++)
-        {
-            shiftRegister.writeByte(i);
-            shiftRegister.latch();
-            _delay_ms(1000);
-        }
+        const uint8_t Five  = 0x6D;
+        const uint8_t Nine  = 0x6F;
+
+        uint8_t character = i % 2 ? Five : Nine;
+        shiftRegister.writeByte(~character);
+        shiftRegister.latch();
+        i++;
+        _delay_ms(500);
     }
     return 0;
 }
