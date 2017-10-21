94cae26c2f112e9a7b83bb9ce082d2e1bfa91fd9
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2017-10-21 11:29:57 -0400

    Added seven segment driver demo

--- src/Demo.cpp
+++ src/Demo.cpp
@@ -1,8 +1,9 @@
 #include "ShiftRegister.h"
+#include "SevenSegment.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
-// /* Shift Register Driver Test 1 */
+// /* Shift Register Driver Demo 1 */
 // int main(void) 
 // {   
 //     IOPort_t* shiftRegisterPort = (IOPort_t*)&DDRB;
@@ -19,22 +20,46 @@
 //     return 0;
 // }
 
+// /* Shift Register Demo 2 */
+// int main(void) 
+// {   
+//     IOPort_t* shiftRegisterPort = (IOPort_t*)&DDRB;
+//     ShiftRegister shiftRegister(*shiftRegisterPort);
+
+//     int i = 0;
+//     for(;;)
+//     {
+//         const uint8_t Five  = 0x6D;
+//         const uint8_t Nine  = 0x6F;
+
+//         uint8_t character = i % 2 ? Five : Nine;
+//         shiftRegister.writeByte(~character);
+//         shiftRegister.latch();
+//         i++;
+//         _delay_ms(500);
+//     }
+//     return 0;
+// }
+
+/* 7-Seg Driver Demo */
 int main(void) 
 {   
     IOPort_t* shiftRegisterPort = (IOPort_t*)&DDRB;
     ShiftRegister shiftRegister(*shiftRegisterPort);
 
-    int i = 0;
+    SevenSegment sevenSeg(shiftRegister);
+
+    const uint8_t Characters[] = {'0','1','2','3','4','5','6','7','8','9','C','F'};
+
     for(;;)
     {
-        const uint8_t Five  = 0x6D;
-        const uint8_t Nine  = 0x6F;
-
-        uint8_t character = i % 2 ? Five : Nine;
-        shiftRegister.writeByte(~character);
-        shiftRegister.latch();
-        i++;
-        _delay_ms(500);
+        int8_t i;
+        for(i = 0; i < 12; i++)
+        {
+          sevenSeg.write(Characters[i]);
+          sevenSeg.display();
+          _delay_ms(1000);
+        }
     }
     return 0;
 }
