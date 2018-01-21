5f055f86ccccace1950a59042b77218285626ccd
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2018-01-21 13:55:59 -0500

    Add onewire demo

--- src/Demo.cpp
+++ src/Demo.cpp
@@ -2,6 +2,8 @@
 #include "SevenSegment.h"
 #include <avr/io.h>
 #include <util/delay.h>
+#include "OneWireImpl.h"
+#include "OneWire.h"
 
 // /* Shift Register Driver Demo 1 */
 // int main(void) 
@@ -41,24 +43,65 @@
 //     return 0;
 // }
 
-/* 7-Seg Driver Demo */
+// /* 7-Seg Driver Demo */
+// int main(void) 
+// {   
+//     IOPort_t* shiftRegisterPort = (IOPort_t*)&PINB;
+//     ShiftRegister shiftRegister(*shiftRegisterPort);
+
+//     SevenSegment sevenSeg(shiftRegister);
+
+//     const uint8_t Characters[] = {'0','1','2','3','4','5','6','7','8','9','C','F'};
+
+//     for(;;)
+//     {
+//         int8_t i;
+//         for(i = 0; i < 12; i++)
+//         {
+//           sevenSeg.write(Characters[i]);
+//           sevenSeg.display();
+//           _delay_ms(1000);
+//         }
+//     }
+//     return 0;
+// }
+
+/* OneWire Demo (best viewed w/ logic analyzer) */
 int main(void) 
 {   
-    IOPort_t* shiftRegisterPort = (IOPort_t*)&PINB;
-    ShiftRegister shiftRegister(*shiftRegisterPort);
+    const uint8_t CMD_SKIP_ROM = 0xCC;
+    const uint8_t CMD_CONVERTT = 0x44;
+    const uint8_t CMD_READ_SCRATCHPAD = 0xBE;
 
-    SevenSegment sevenSeg(shiftRegister);
-
-    const uint8_t Characters[] = {'0','1','2','3','4','5','6','7','8','9','C','F'};
+    IOPort_t* oneWirePort = (IOPort_t*)&PIND;
+    OneWireImpl impl(*oneWirePort, PIND7);
+    OneWire oneWire(impl);
 
     for(;;)
     {
-        int8_t i;
-        for(i = 0; i < 12; i++)
+        oneWire.reset();
+        bool deviceFound = oneWire.devicePresent();
+
+        oneWire.write(CMD_SKIP_ROM);
+        oneWire.write(CMD_CONVERTT);
+
+        //todo: create an abstraction for waiting for conversion
+        impl.issueReadSlot();
+        while(!impl.readPin()) {
+            //todo: use an interrupt instead
+            //todo: add a timeout?
+        }
+
+        oneWire.reset();
+        deviceFound = oneWire.devicePresent();
+
+        oneWire.write(CMD_SKIP_ROM);
+        oneWire.write(CMD_READ_SCRATCHPAD);
+
+        uint8_t buffer[9];
+        for(int i = 8; i >= 0; i--)
         {
-          sevenSeg.write(Characters[i]);
-          sevenSeg.display();
-          _delay_ms(1000);
+            buffer[i] = oneWire.read();
         }
     }
     return 0;
