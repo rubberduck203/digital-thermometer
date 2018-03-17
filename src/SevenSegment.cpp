7a0ca6ee483b7aa561ac9472ab3ba29a8c220d8d
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2018-03-17 11:26:44 -0400

    Revert "Inject hang to verify afl is works."
    This reverts commit be4452c01f1012d96b7e73466bed22805302e712.

--- src/SevenSegment.cpp
+++ src/SevenSegment.cpp
@@ -96,7 +96,6 @@ void SevenSegment::write(uint8_t character)
             data = 0x40;
             break;
         default:
-            for(;;){}
             data = 0x00; // turn the segment off if we get an unsupported char
     }
 
